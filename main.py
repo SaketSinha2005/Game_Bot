be5aaf880d40838abc52b9ffa22c0eb0653bf760
Saket Kumar Sinha <ss691@snu.edu.in>
2026-02-06 23:05:09 +0530

    update

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
-"""
-A test runner.
-
-HOW TO USE:
-1. Create your bot in submissions/yourname_yournetid.py
-2. Change the import line to import YOUR bot
-3. Run python main.py
-"""
-
-from pingv4 import Connect4Game, MinimaxBot, RandomBot
-from submissions import template_bot.py # Change this line to import yourname_yournetid.py
+from pingv4 import Connect4Game, MinimaxBot
+from submissions.saket_ss691 import ss691
 
 def main():
-  bot = Bot()
-  print("=" * 50)
-  print(f"Testing Bot: {bot.strategy_name}")
-  print(f"Author: {bot.author_name} {bot.author_netid}")
-  print("=" * 50)
-  print()
+    bot = ss691   # IMPORTANT
 
-  # Test 1: Human vs Your Bot
-  print("Test: Human vs Your Bot")
-  input("Press Enter to start")
-  game = Connect4Game(player1=None, player2=Bot)
-  game.run()
+    print("=" * 50)
+    print(f"Testing Bot: {bot.strategy_name}")
+    print(f"Author: {bot.author_name} {bot.author_netid}")
+    print("=" * 50)
 
-  # Uncomment additional tests
-
-  # Test 3: Your Bot vs Random Bot
-  # print("Test: Your Bot vs Random Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=RandomBot")
-  # game.run()
-
-  # Test 34: Your Bot vs Minimax Bot
-  # print("Test: Your Bot vs Minimax Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=MinimaxBot")
-  # game.run()
-
-  # Test 3: Your Bot vs Your Bot
-  # print("Test: Your Bot vs Your Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=Bot")
-  # game.run()
+    input("Press Enter to start")
+    game = Connect4Game(player1=MinimaxBot, player2=bot)
+    game.run()
 
 if __name__ == "__main__":
-  main()
+    main()
